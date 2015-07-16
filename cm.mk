f2e2d3444452e5ff91e37c031152a3a9e5e7c88d
Sebastian Jena <sebastianjena95@gmail.com>
2015-07-16 11:11:40 +0200

    Initial commit

---
+++ cm.mk
@@ -0,0 +1,18 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := a7600f
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lenovo/a7600f/device_a7600f.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := a7600f
+PRODUCT_NAME := cm_a7600f
+PRODUCT_BRAND := lenovo
+PRODUCT_MODEL := a7600f
+PRODUCT_MANUFACTURER := lenovo
